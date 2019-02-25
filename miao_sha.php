23c63d0b49646f16e7b49991ea5f6682aff36c16
jack <904916790@qq.com>
2019-02-25 19:11:12 +0800

    tmp

--- miao_sha.php
+++ miao_sha.php
@@ -1,14 +1,12 @@
 <?php
 
 /*
-$redis = new Redis;
-$redis->connect('127.0.0.1', 6379);
-$redis->set("myfirst", 200);
-
-$result = $redis->get('myfirst');
-var_dump($result);
+version 1
+手动设置redis的key，myfirst为10，也就是模拟200个人抢10个商品
+发现结果中有超过10个succ，也就是超发了
 */
 
+/*
 $times = 200;
 while($times-- > 0){
 	$pid = pcntl_fork();
@@ -32,5 +30,32 @@ function test(){
 	}
 	exit();
 }
+*/
 
 
+/*
+
+*/
+$times = 200;
+while($times-- > 0){
+	$pid = pcntl_fork();
+	if($pid > 0){
+		test();
+		exit;
+	}
+	
+}
+
+function test(){
+	usleep(50000);
+	$redis = new Redis;
+	$redis->connect('127.0.0.1', 6379);
+	$left = $redis->get("myfirst");
+	if($left > 0){
+		$redis->decr('myfirst');
+		echo 'succ'."\n";
+	}else{
+		echo 'failed'."\n";
+	}
+	exit();
+}
